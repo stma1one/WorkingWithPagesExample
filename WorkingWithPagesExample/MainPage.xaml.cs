b993a985e096e578e61cab7bc5ce3853fe71d0b2
Home <Home@LAPTOP-2E6V6ACM>
2021-02-25 14:05:11 +0200

    added some dynamic lables and layouts

--- WorkingWithPagesExample/MainPage.xaml.cs
+++ WorkingWithPagesExample/MainPage.xaml.cs
@@ -10,58 +10,68 @@ namespace WorkingWithPagesExample
 {
     public partial class MainPage : ContentPage
     {
-        Label txtLbl;
-        Label TxtLbl2;
+       
+        Label LblMsg;
         Button btnNext;
-        StackLayout stck;
+        
 
         public MainPage()
         {
             InitializeComponent();
             InitializeLayout();
-            InitializeLabel();
-            InitializeButton();
+            
+           
         }
 
         private void InitializeLayout()
         {
-            stck = new StackLayout();
-            stck.BackgroundColor = Color.BlueViolet;
-            stck.Margin = 20;
-            Content = stck;
+            btnNext = new Button { Text = "Next" };
+
+            btnNext.Clicked+= (sender, e) => { ((App)this.Parent).MainPage = new SubPage(); };
+            LblMsg = new Label
+            {
+                Text = " ",
+                FontSize = 20,
+                HorizontalOptions=LayoutOptions.CenterAndExpand,
+                TextColor = Color.Red
+            };
+
+            Content = new StackLayout
+            {
+                BackgroundColor = Color.BlueViolet,
+                Margin = 20,
+                Children =
+                {
+                    new StackLayout
+                    {
+                        BackgroundColor=Color.DarkSeaGreen,
+                        Margin=20,
+                        Children=
+                        {
+                          new Label{Text = "לחץ על הכפתור למעבר לדף הבא",
+                          TextColor = Color.LightYellow,
+                          FontSize = 25 },
+                          btnNext,
+                          LblMsg
+                        }
+                        
+                            
+                    }
+                    
+
+                }
+            };
+            
+            
+           
         }
 
-        private void InitializeButton()
-        {
-            btnNext = new Button();
-            btnNext.Text = "Next";
-            btnNext.Clicked += (sender, e) => { ((App)this.Parent).MainPage = new SubPage();  };
-            if (stck == null)
-                InitializeLayout();
-             stck.Children.Add(btnNext);
+       
 
-        }
-
-        private void InitializeLabel()
-        {
-            txtLbl = new Label();
-            txtLbl.Text = "לחץ על הכפתור למעבר לדף הבא";
-            txtLbl.TextColor = Color.LightYellow;
-            txtLbl.FontSize = 25;
-            TxtLbl2 = new Label();
-            TxtLbl2.Text = " ";
-            TxtLbl2.TextColor = Color.LightYellow;
-            TxtLbl2.FontSize = 15;
-
-            if (stck == null)
-                InitializeLayout();
-            stck.Children.Add(txtLbl);
-            stck.Children.Add(TxtLbl2);
-
-        }
+       
         protected override void OnDisappearing()
         {
-            TxtLbl2.Text+= $"\nYou left the main page on {DateTime.Now.ToString()}";    
+            LblMsg.Text+= $"\nYou left the main page on {DateTime.Now.ToString()}";    
         }
     }
 }
